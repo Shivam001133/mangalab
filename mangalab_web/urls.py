b6df3257795ddc2086fcb0c2500c6b75a7e9b0e8
Shivam <skhivampandey00113@fgmail.com>
2024-04-13 00:51:55 +0530

    removed templates

--- mangalab_web/urls.py
+++ mangalab_web/urls.py
@@ -7,12 +7,12 @@ from mangalab_web import views
 app_name = 'web'
 
 urlpatterns = [
-    path('menu-title/', apis.menu_title, name='menu_title'),
-    # path('categories/', apis.manga_categories, name='manga_categories'),
+    # path('menu-title/', apis.menu_title, name='menu_title'),
+    path('categories/', apis.manga_categories, name='manga_categories'),
     path('', views.indexView, name='home-page'),
-    path('deatil/', views.animeDetailView, name='anime-detail'),
-    path('watch/', views.amimeWatchView, name='anime-watch'),
-    path('blog/', views.blogView, name='blog'),
-    path('blog-detail/', views.blogDetailView, name='blog-detail'),
-    path('categories/', views.categoriesView, name='catego;ries')
+    # path('deatil/', views.animeDetailView, name='anime-detail'),
+    # path('watch/', views.amimeWatchView, name='anime-watch'),
+    # path('blog/', views.blogView, name='blog'),
+    # path('blog-detail/', views.blogDetailView, name='blog-detail'),
+    # path('categories/', views.categoriesView, name='catego;ries')
     ]
--- mangalab_web/views.py
+++ mangalab_web/views.py
@@ -2,36 +2,36 @@ from django.shortcuts import render
 from manga.models import MangaList, TitleImage
 
 
-def indexView(request):
-    data = []
-    mangas = list(TitleImage.objects.all())
-    print(mangas)
-    for img in mangas[:7]:
-        data.append({
-            "title": img.manga.title,
-            "img": img.image
-        })
-    print(data)
-    for i in data:
-        print(f"**** {i}")
+# def indexView(request):
+#     data = []
+#     mangas = list(TitleImage.objects.all())
+#     print(mangas)
+#     for img in mangas[:7]:
+#         data.append({
+#             "title": img.manga.title,
+#             "img": img.image
+#         })
+#     print(data)
+#     for i in data:
+#         print(f"**** {i}")
 
-    return render(request, 'anime-main/index.html', context={"context": data})
+#     return render(request, 'anime-main/index.html', context={"context": data})
 
-def animeDetailView(request):
-    return render(request, 'anime-main/anime-details.html')
+# def animeDetailView(request):
+#     return render(request, 'anime-main/anime-details.html')
 
-def amimeWatchView(request):
-    return render(request, 'anime-main/anime-watching.html')
+# def amimeWatchView(request):
+#     return render(request, 'anime-main/anime-watching.html')
 
-def blogView(request):
-    return render(request, 'anime-main/blog.html')
+# def blogView(request):
+#     return render(request, 'anime-main/blog.html')
 
-def blogDetailView(request):
-    return render(request, 'anime-main/blog-details.html')
+# def blogDetailView(request):
+#     return render(request, 'anime-main/blog-details.html')
 
 
-def categoriesView(request):
-    return render(request, 'anime-main/categories.html')
+# def categoriesView(request):
+#     return render(request, 'anime-main/categories.html')
 
 # def amimeWatchView(request):
 #     return render(request, 'anime-main/anime-watching.html')
